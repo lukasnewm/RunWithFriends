7819d439585f57679bc30cc501669ca465455730
lukasnewm <lukas@newmansplace.com>
2020-05-15 23:00:20 -0400

    Correct mapView set up

--- app/src/main/java/com/lukasnewman/runwithfriends/ui/workout/WorkoutFragment.kt
+++ app/src/main/java/com/lukasnewman/runwithfriends/ui/workout/WorkoutFragment.kt
@@ -3,7 +3,6 @@ package com.lukasnewman.runwithfriends.ui.workout
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,26 +10,22 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.MapsInitializer
-import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.lukasnewman.runwithfriends.R
 import kotlinx.android.synthetic.main.fragment_workout.*
-import kotlinx.android.synthetic.main.fragment_workout.view.*
 
 class WorkoutFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var workoutViewModel: WorkoutViewModel
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
-    //private lateinit var googleMap: GoogleMap
-    private var mapView: MapView = requireView().findViewById(R.id.mapView)
-    private var mapsSupported = true;
+    private lateinit var locationRequest: LocationRequest
+    private var mapView: MapView? = null
+    private var googleMap: GoogleMap? = null
+    private var requestingLocationUpdates = false
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -52,22 +47,12 @@ class WorkoutFragment : Fragment(), OnMapReadyCallback {
 
         mapView = view.findViewById(R.id.mapView)
         if (mapView != null) {
-            mapView.onCreate(null)
-            mapView.onResume()
-            mapView.getMapAsync(this)
+            mapView!!.onCreate(null)
+            mapView!!.onResume()
+            mapView!!.getMapAsync(this)
         }
 
-        //Figure out what his means for getting constant location
-        locationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult) {
-                locationResult ?: return
-                for (location in locationResult.locations) {
-                    //Update UI with location data
-                }
-            }
-        }
-
-        //Start the workout
+        //Start the workout button press
         buttonStart.setOnClickListener(View.OnClickListener { v: View ->
 
         })
@@ -77,33 +62,35 @@ class WorkoutFragment : Fragment(), OnMapReadyCallback {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        //Get users current location
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity().applicationContext)
-        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-            if (location != null) {
-                Toast.makeText(activity, location.toString(), Toast.LENGTH_LONG).show()
-
-                //Users Location on the map
-
-            } else {
-                //Toast.makeText(activity, "Location Unsuccesful", Toast.LENGTH_SHORT).show()
-
-
-
-            }
-        }
+        //getTheLastLocation()
     }
 
     override fun onMapReady(p0: GoogleMap?) {
         MapsInitializer.initialize(context)
-        //googleMap = p0
+        googleMap = p0
     }
 
-    //private fun startLocationUpdates() {
-    //    fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
-    //}
+    private fun getTheLastLocation() {
+        //Get users last known location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity().applicationContext)
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                //Users Location on the map
+                var loc = LatLng(location.latitude, location.longitude)
+                googleMap!!.animateCamera(CameraUpdateFactory.newLatLng(loc))
+                googleMap!!.addMarker(MarkerOptions().position(loc))
+            } else {
+                //Toast.makeText(activity, "Location Unsuccesful", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
-    //private fun stopLocationUpdates() {
-    //    fusedLocationClient.removeLocationUpdates(locationCallback)
-    //}
+    private fun startLocationUpdates() {
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (requestingLocationUpdates) startLocationUpdates()
+    }
 }
